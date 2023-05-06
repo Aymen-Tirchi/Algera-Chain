df7a41cfe61f3725c615355c17c9f85add0f21e5
Youcef <mac@MacBook-Air-de-Mac.local>
2023-05-06 23:37:23 +0100

    new desgin of home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { useStyles } from "../components/Styles";
 import Grid from "@material-ui/core/Grid";
-
+import { Link } from "react-router-dom";
+import AboutUs from "../components/AboutUs";
 
 export default function Home() {
   const classes = useStyles();
   const navItem = [];
 
+  const [currentCard, setCurrentCard] = useState(0);
+
+  const handleNextCard = () => {
+    setCurrentCard((currentCard + 1) % cards.length);
+  };
+
+  const handlePrevCard = () => {
+    setCurrentCard((currentCard + cards.length - 1) % cards.length);
+  };
+
   return (
     <>
       <div className={classes.pageWrap}>
         <Navbar navItems={navItem}>
+          <h1>This is the home page</h1>
           <Grid
             container
             spacing={3}
@@ -30,11 +42,14 @@ export default function Home() {
                 flexWrap: "wrap",
               }}
             >
-           
             
-             
-
+              <img
+                alt="."
+                src="/track.png"
+                style={{ width: "75%", height: "auto" }}
+              />
             </Grid>
+            
 
             <Grid
               item
@@ -49,17 +64,23 @@ export default function Home() {
                 flexWrap: "wrap",
                 flexDirection: "column",
               }}
-                // change and animation would be here
-              >
-               
-              <h1 className={classes.pageHeading}>  . </h1>
-                        
+            >
+              <h1 className={classes.pageHeading}>Set Roles</h1>
+              
             </Grid>
           </Grid>
         </Navbar>
+        <div>
+        
+        <h1>This is the description of what is Algeria Chain </h1>
+        <p>Some more content here...</p>
+        <h2> Contact us part</h2>
+        <p>Phone: 123-456-7890</p>
+        <p>Location: New York, NY</p>
+      </div>
         <br />
 
-         
+                <AboutUs />
       </div>
     </>
   );
